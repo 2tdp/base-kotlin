ef0ed12556a9ad774fec85bf017710877fcb6bc0
2tdp <datnt.080498kc@gmail.com>
2023-10-25 16:26:23 +0700

    updates

--- app/src/main/java/com/remi/ringtones/audiocutter/ringtonemaker/freeringtone/extensions/ImageView.kt
+++ app/src/main/java/com/remi/ringtones/audiocutter/ringtonemaker/freeringtone/extensions/ImageView.kt
@@ -2,10 +2,12 @@ package com.remi.ringtones.audiocutter.ringtonemaker.freeringtone.extensions
 
 import android.content.Context
 import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.content.ContextCompat
 import com.remi.ringtones.audiocutter.ringtonemaker.freeringtone.utils.designvector.VectorChildFinder
 import com.remi.ringtones.audiocutter.ringtonemaker.freeringtone.utils.designvector.VectorDrawableCompat
 
-fun ImageView.changeColorIcon(context: Context, namePath: String, quantityPath: Int, resPath: Int,
+fun AppCompatImageView.changeColorIcon(context: Context, namePath: String, quantityPath: Int, resPath: Int,
                          colorBackground: Int, color: Int) {
 
     var vectorChild: VectorChildFinder? = null
@@ -17,4 +19,8 @@ fun ImageView.changeColorIcon(context: Context, namePath: String, quantityPath: 
         path = vectorChild.findPathByName(namePath + (i + 1))
         if (i == 0) path.fillColor = colorBackground else path.fillColor = color
     }
+}
+
+fun AppCompatImageView.tintColor(context: Context, color: Int) {
+    imageTintList = ContextCompat.getColorStateList(context, color)
 }
